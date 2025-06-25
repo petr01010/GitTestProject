348aba91a9c020a1ead5d6a2c9ec2fbe4e4f34c2
Petr Jeřábek <petr.jerabek@gmail.com>
2025-06-25 16:18:40 +0200

    Update greeting in feature-branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Feature branch says hi!");
         System.out.println("Hello world! .... A");
         System.out.println("main-B");
     }
