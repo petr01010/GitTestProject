48aac4e85088b70a8830778faa1d567619ccc411
Petr <petr.jerabek@gmail.com>
2025-06-27 11:11:35 +0200

    main-A

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Feature branch says hi!");
-        System.out.println("B");
-        System.out.println("C");
-        System.out.println("Hello world! .... A");
-        System.out.println("main-B");
+        System.out.println("A");
     }
 }
