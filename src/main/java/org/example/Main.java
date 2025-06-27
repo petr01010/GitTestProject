7687fe928f7c79eb1f0f2182189a2198dba45c9a
Petr <petr.jerabek@gmail.com>
2025-06-27 08:42:37 +0200

    main-B

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! .... A");
+        System.out.println("main-B");
     }
 }
