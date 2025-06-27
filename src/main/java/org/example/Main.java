de2bb76646aa857e1ff42a7db984ec796f8e9d78
Petr <petr.jerabek@gmail.com>
2025-06-27 12:44:59 +0200

    Z

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("D");
         System.out.println("E");
         System.out.println("F");
-        System.out.println("G");
+        System.out.println("Z");
         System.out.println("H");
     }
 }
